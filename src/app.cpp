5e4e70e26f23c71943dcbc04147a15abe53b39c3
st0rmbtw <>
2025-06-02 15:12:16 +0300

    Some changes

--- src/app.cpp
+++ src/app.cpp
@@ -23,16 +23,17 @@ using namespace sge;
 static constexpr size_t PARTICLE_COUNT = 4000;
 static constexpr float PARTICLE_SIZE = 8.0f;
 static constexpr float PARTICLE_SPACING = 5.0f;
-static constexpr float SMOOTHING_RADIUS = 15.0f;
-static constexpr float KERNEL_H = 2.0f;
-static constexpr float GRAVITY = 150.0f;
-static constexpr float TARGET_DENSITY = 6.0f;
-static constexpr float PRESSURE_MULTIPLIER = 150.0f;
-static constexpr float NEAR_PRESSURE_MULTIPLIER = 100.0f;
+static constexpr float SMOOTHING_RADIUS = PARTICLE_SIZE;
+static constexpr float KERNEL_H = 1.5f;
+static constexpr float GRAVITY = 0.0f;
+static constexpr float PRESSURE_MULTIPLIER = 500.0f;
+static constexpr float NEAR_PRESSURE_MULTIPLIER = PRESSURE_MULTIPLIER / 15.0f;
 static constexpr float COLLISION_DAMPING = 0.9f;
 static constexpr float VISCOSITY_STRENGTH = 0.1f;
 static constexpr float INTERACTION_RADIUS = 250.0f;
-static constexpr float INTERACTION_STRENGTH = 500.0f;
+static constexpr float INTERACTION_STRENGTH = PRESSURE_MULTIPLIER * 2.0f;
+static constexpr float MASS = 1000.0f * PARTICLE_SIZE * PARTICLE_SIZE;
+static constexpr float TARGET_DENSITY = 4.0f * MASS;
 
 struct Particle {
     glm::vec2 position;
@@ -54,16 +55,21 @@ static struct GameState {
     BS::thread_pool<> pool;
 } g;
 
-float ExampleFunction(glm::vec2 pos) {
-    return glm::cos((pos.y / 80.0f) - 3.0f + glm::sin(pos.x / 80.0f)) * 0.5f + 0.5f;
-    // return glm::cos(pos.x / 100.0f) * 0.5f + 0.5f;
+static constexpr float PI = glm::pi<float>();
+
+float Poly6Kernel(float dst, float radius) {
+    if (dst >= radius) return 0.0f;
+
+    float v = (radius*radius) - (dst*dst);
+    float scale = 4.0f / (PI * glm::pow(radius, 8.0f));
+    return v * v * v * scale;
 }
 
-float DensityKernel(float x, float radius) {
-    if (x >= radius) return 0.0f;
+float SpikyKernelDerivative(float dst, float radius) {
+    if (dst >= radius) return 0.0f;
 
-    float v = radius - x;
-    float scale = 6.0f / (glm::pi<float>() * glm::pow(radius, 4.0f));
+    float v = radius - dst;
+    float scale = -30.0f / (PI * glm::pow(radius, 5.0f));
     return v * v * scale;
 }
 
@@ -71,14 +77,24 @@ float NearDensityKernel(float x, float radius) {
     if (x >= radius) return 0.0f;
 
     float v = radius - x;
-    float scale = 10.0f / (glm::pi<float>() * glm::pow(radius, 5.0f));
+    float scale = 10.0f / (PI * glm::pow(radius, 5.0f));
     return v * v * v * scale;
 }
 
-float DensityKernelDerivative(float x, float radius) {
-    if (x >= radius) return 0.0f;
+float Poly6KernelDerivative(float dst, float radius) {
+    // const float ratio = dst / radius;
+    // if (ratio > 2.0f) {
+    //     return 0.0f;
+    // }
+    // const float normalization = 15.0f / (14.0f * PI * radius*radius);
+    // if (0.0f <= ratio && ratio < 1.0f) {
+    //     return normalization * (9.0f * ratio*ratio - 12.0 * ratio);
+    // }
+    // return 3.0f * normalization * (2.0f - ratio) * (2.0f - ratio);
 
-    float v = radius - x;
+    if (dst >= radius) return 0.0f;
+
+    float v = radius - dst;
     float scale = 12.0f / (glm::pow(radius, 4.0f) * glm::pi<float>());
     return -v * scale;
 }
@@ -87,20 +103,41 @@ float NearDensityKernelDerivative(float x, float radius) {
     if (x >= radius) return 0.0f;
 
     float v = radius - x;
-    float scale = 30.0f / (glm::pow(radius, 5.0f) * glm::pi<float>());
+    float scale = 30.0f / (glm::pow(radius, 5.0f) * PI);
     return -v * v * scale;
 }
 
-float ViscosityKernel(float x, float radius) {
-    if (x >= radius) return 0.0f;
+float ViscosityKernel(float dst, float radius) {
+    if (dst >= radius) return 0.0f;
 
-    float v = radius * radius - x * x;
-    float q = SMOOTHING_RADIUS;
-    float q6 = glm::pow(q, 6.0f);
-    float q4 = glm::pow(q, 4.0f);
-    float q2 = glm::pow(q, 2.0f);
-    float scale = (4.0f * q6) / (glm::pi<float>() * (4.0f*q6 - 6.0f * q4 + 4.0f * q2 - 1.0f) * glm::pow(radius, 8.0f));
-    return v * v * v * scale;
+    float v = radius - dst;
+    float scale = 40.0f / (PI * glm::pow(radius, 5.0f));
+    return v * scale;
+}
+
+float SpikySmoothingKernel(float dst, float radius) {
+    if (dst >= radius) {
+        return 0.0f;
+    }
+    const float volume = PI * glm::pow(radius, 6.0f) / 15.0f;
+    const float v = (radius - dst);
+    return v * v * v / volume;
+}
+
+float SbSmoothingKernel(float dst, float radius) {
+    if (dst >= radius) {
+        return 0.0f;
+    }
+    const float v = radius - dst;
+
+    return v * v * 6.0f / (PI * glm::pow(radius, 4.0f));
+}
+
+float sb_smoothing_kernel_derivative(float dst, float radius) {
+    if (dst >= radius) {
+        return 0.0f;
+    }
+    return (dst - radius) * 12.0 / (PI * glm::pow(radius, 4.0f));
 }
 
 std::tuple<float, float> CalculateDensity(size_t index) {
@@ -109,15 +146,17 @@ std::tuple<float, float> CalculateDensity(size_t index) {
 
     const glm::vec2 point = g.predicted_positions[index];
 
+    static constexpr float SQR_RADIUS = (SMOOTHING_RADIUS * KERNEL_H) * (SMOOTHING_RADIUS * KERNEL_H);
+
     for (size_t i = 0; i < PARTICLE_COUNT; ++i) {
         glm::vec2 offset = g.predicted_positions[i] - point;
         float sqr_dst = glm::dot(offset, offset);
-        if (sqr_dst > SMOOTHING_RADIUS * SMOOTHING_RADIUS) continue;
+        if (sqr_dst > SQR_RADIUS) continue;
 
         float dst = glm::sqrt(sqr_dst);
 
-        density += DensityKernel(dst, SMOOTHING_RADIUS);
-        near_density += NearDensityKernel(dst, SMOOTHING_RADIUS);
+        density += Poly6Kernel(dst, SMOOTHING_RADIUS * KERNEL_H) * MASS;
+        near_density += NearDensityKernel(dst, SMOOTHING_RADIUS * KERNEL_H) * MASS;
     }
 
     return std::make_tuple(density, near_density);
@@ -186,13 +225,15 @@ static glm::vec2 CalculatePressureForce(size_t index) {
 
     const glm::vec2 point = g.predicted_positions[index];
 
+    static constexpr float SQR_RADIUS = (SMOOTHING_RADIUS * KERNEL_H) * (SMOOTHING_RADIUS * KERNEL_H);
+
     for (size_t i = 0; i < PARTICLE_COUNT; ++i) {
         if (i == index) continue;
 
         glm::vec2 offset = g.predicted_positions[i] - point;
         float sqr_dst = glm::dot(offset, offset);
 
-        if (sqr_dst > SMOOTHING_RADIUS * SMOOTHING_RADIUS) continue;
+        if (sqr_dst > SQR_RADIUS) continue;
 
         float dst = glm::sqrt(sqr_dst);
         glm::vec2 dir = dst > glm::epsilon<float>() ? (g.predicted_positions[i] - point) / dst : glm::linearRand(glm::vec2(-1.0f), glm::vec2(1.0f));
@@ -203,8 +244,11 @@ static glm::vec2 CalculatePressureForce(size_t index) {
         float shared_pressure = CalculateSharedPressure(g.densities[index], density);
         float shared_near_pressure = CalculateSharedNearPressure(g.near_densities[index], near_density);
 
-        pressure_force += shared_pressure * dir * DensityKernelDerivative(dst, SMOOTHING_RADIUS) / density;
-        pressure_force += shared_near_pressure * dir * NearDensityKernelDerivative(dst, SMOOTHING_RADIUS) / near_density;
+        float slope = SpikyKernelDerivative(dst, SMOOTHING_RADIUS * KERNEL_H);
+        float near_slope = NearDensityKernelDerivative(dst, SMOOTHING_RADIUS * KERNEL_H);
+
+        pressure_force += shared_pressure * dir * slope * MASS / density;
+        pressure_force += shared_near_pressure * dir * near_slope * MASS / near_density;
     }
 
     return pressure_force;
@@ -241,15 +285,18 @@ static glm::vec2 CalculateViscosityForce(size_t index) {
 
     const glm::vec2 point = g.positions[index];
 
+    static constexpr float SQR_RADIUS = (SMOOTHING_RADIUS * KERNEL_H) * (SMOOTHING_RADIUS * KERNEL_H);
+
     for (size_t i = 0; i < PARTICLE_COUNT; ++i) {
         glm::vec2 offset = g.positions[i] - point;
         float sqr_dst = glm::dot(offset, offset);
 
-        if (sqr_dst > SMOOTHING_RADIUS * SMOOTHING_RADIUS) continue;
+        if (sqr_dst > SQR_RADIUS) continue;
 
         float dst = glm::sqrt(sqr_dst);
-        float influence = ViscosityKernel(dst / SMOOTHING_RADIUS, KERNEL_H);
-        viscosity_force += (g.velocities[i] - g.velocities[index]) * influence;
+        float influence = ViscosityKernel(dst, SMOOTHING_RADIUS * KERNEL_H);
+        float density = g.densities[i];
+        viscosity_force += (g.velocities[i] - g.velocities[index]) * MASS * influence / density;
     }
 
     return viscosity_force * VISCOSITY_STRENGTH;
@@ -259,46 +306,31 @@ static void ResolveCollisions(glm::vec2& position, glm::vec2& velocity) {
     const float width = g.camera.viewport().x;
     const float height = g.camera.viewport().y;
 
-    if (position.x + PARTICLE_SIZE > width) {
-        position.x = width - PARTICLE_SIZE;
+    const float half_size = PARTICLE_SIZE / 2.0f;
+
+    if (position.x + half_size > width) {
+        position.x = width - half_size;
         velocity.x *= -1.0f * COLLISION_DAMPING;
     }
 
-    if (position.y + PARTICLE_SIZE > height) {
-        position.y = height - PARTICLE_SIZE;
+    if (position.y + half_size > height) {
+        position.y = height - half_size;
         velocity.y *= -1.0f * COLLISION_DAMPING;
     }
 
-    if (position.x < 0) {
-        position.x = 0;
+    if (position.x - half_size < 0) {
+        position.x = half_size;
         velocity.x *= -1.0f * COLLISION_DAMPING;
     }
 
-    if (position.y < 0) {
-        position.y = 0;
+    if (position.y - half_size < 0) {
+        position.y = half_size;
         velocity.y *= -1.0f * COLLISION_DAMPING;
     }
 }
 
 void pre_update() {
 
-}
-
-
-template <size_t SIZE>
-LinearRgba MultiColorLerp(const LinearRgba (&colors)[SIZE], float t) {
-    t = glm::clamp(t, 0.0f, 1.0f);
-
-    float delta = 1.0f / (SIZE - 1);
-    int startIndex = (int)(t / delta);
-
-    if (startIndex == SIZE - 1) {
-        return colors[SIZE - 1];
-    }
-
-    float localT = std::fmod(t, delta) / delta;
-
-    return (colors[startIndex] * (1.0f - localT)) + (colors[startIndex + 1] * localT);
 }
 
 struct GradientKey {
@@ -340,23 +372,21 @@ void fixed_update() {
     g.pool.submit_loop(0, PARTICLE_COUNT, [dt](size_t i) {
         g.velocities[i] += CalculateExternalForces(i) * dt;
         g.predicted_positions[i] = g.positions[i] + g.velocities[i] * dt;
+
         auto [density, near_density] = CalculateDensity(i);
         g.densities[i] = density;
         g.near_densities[i] = near_density;
     }).wait();
 
     g.pool.submit_loop(0, PARTICLE_COUNT, [dt](size_t i) {
-        glm::vec2 pressure_force = CalculatePressureForce(i);
-        glm::vec2 pressure_accel = pressure_force / g.densities[i];
-        g.velocities[i] += pressure_accel * dt;
+        g.velocities[i] += CalculatePressureForce(i) / g.densities[i] * dt;
     }).wait();
 
     g.pool.submit_loop(0, PARTICLE_COUNT, [dt](size_t i) {
-        glm::vec2 viscosity_force = CalculateViscosityForce(i);
-        g.velocities[i] += viscosity_force * dt;
+        g.velocities[i] += CalculateViscosityForce(i) * dt;
     }).wait();
 
-    float velocity_max = 8.0f;
+    float velocity_max = 10.0f;
     for (size_t i = 0; i < PARTICLE_COUNT; ++i) {
         const glm::vec2& v = g.velocities[i];
         velocity_max = std::max(velocity_max, glm::dot(v, v));
@@ -386,11 +416,19 @@ void render() {
 
     g.batch.BeginOrderMode();
     {
+        g.batch.DrawCircle(Input::MouseScreenPosition(), {
+            .radius = SMOOTHING_RADIUS,
+            .color = LinearRgba::transparent(),
+            .border_thickness = 1.0f,
+            .border_color = LinearRgba::red()
+        });
+
         for (size_t i = 0; i < PARTICLE_COUNT; ++i) {
-            g.batch.DrawCircle(g.positions[i], {
+            const glm::vec2 pos = g.positions[i];
+            g.batch.DrawCircle(pos, {
                 .radius = PARTICLE_SIZE / 2.0f,
                 .color = g.colors[i],
-                .anchor = Anchor::TopLeft
+                .anchor = Anchor::Center
             });
         }
     }
